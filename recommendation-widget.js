a0a6792caa2336635a0cff144bdee716308e5459
Ashley Sullivan <ashley@ashleysullivan.co>
2019-01-25 14:51:21 -0500

    update tracker event code

--- recommendation-widget.js
+++ recommendation-widget.js
@@ -8,4 +8,4 @@ var products = document.querySelectorAll('[dy-needs-disclosure]');
 
 console.log(window.sst)
 
-window.sst.el.sendTrackEvent({ event: 'dy-notification', campaign: '${DY Test Name}', variant: '${trackerVariant}' });
+window.sst.el.sendTrackEvent({ event: '${tracker event}', value: '${DY Test Name}', variant: '${trackerVariant}' });
