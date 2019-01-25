e40dd5c9d784dfa78528cebb358867a44e7f5108
Ashley Sullivan <ashley@ashleysullivan.co>
2019-01-25 14:44:54 -0500

    js for the data-position attribute

--- recommendation-widget.js
+++ recommendation-widget.js
@@ -1,3 +1,9 @@
+// iterates over the data-position
+var position = 0;
+$('.rec_container').each(function(position){
+  $(this).attr('data-position', position++);
+});
+
 var products = document.querySelectorAll('[dy-needs-disclosure]');
 
 console.log(window.sst)
