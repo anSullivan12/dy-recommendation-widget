79ef75f01650d4a74837d40ac36d239e59e91ce3
Ashley Sullivan <ashley@ashleysullivan.co>
2019-01-28 14:01:02 -0500

    remove disclosure stuff

--- recommendation-widget.js
+++ recommendation-widget.js
@@ -4,11 +4,6 @@ $('.rec_container').each(function(position){
   $(this).attr('data-position', position++);
 });
 
-var products = document.querySelectorAll('[dy-needs-disclosure]');
-if (products > 1) {
-  $('#disclosure').addClass('show');
-}
-
 
 console.log(window.sst)
 
